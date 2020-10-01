39fdd177f4c98dae363d2f9ec63b0b1686b1afa0
Mike Manchee <mikemanchee@gmail.com>
2020-09-30 20:44:11 -0700

    add tests and functions for pastry constructor

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -7,6 +7,55 @@ namespace PierresBakery.Tests
   public class PastryTests
   {
     [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry("test", 0, 0);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void GetObject_ReturnsObject_Object()
+    {
+      //Arrange
+      string name = "loaf";
+      int number = 0;
+      int cost = 0;
+
+      //Act
+      Pastry result = new Pastry(name, number, cost);
+
+      //Assert
+      Assert.AreEqual(name, result.Name);
+      Assert.AreEqual(number, result.Number);
+      Assert.AreEqual(cost, result.Cost);
+    }
+
+    [TestMethod]
+    public void SetObject_SetObject_Pastry()
+    {
+      //Arrange
+      string name = "loaf";
+      int number = 0;
+      int cost = 0;
+      Pastry newPastry = new Pastry(name, number, cost);
+
+      //Act
+      string updatedName = "loaves";
+      newPastry.Name = updatedName;
+      string result1 = newPastry.Name;
+      int updatedNumber = 2;
+      newPastry.Number = updatedNumber;
+      int result2 = newPastry.Number;
+      int updatedCost = 2;
+      newPastry.Cost = updatedCost;
+      int result3 = newPastry.Cost;
+
+      //Assert
+      Assert.AreEqual(updatedName, result1);
+      Assert.AreEqual(updatedNumber, result2);
+      Assert.AreEqual(updatedCost, result3);
+    }
+    [TestMethod]
     public void PastryCost_CorrectlyCalcPriceFor1Pastry_2()
     {
       Assert.AreEqual(2, Pastry.PastryCost(1));
